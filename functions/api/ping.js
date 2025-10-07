1d2247b9a21d1a30c6d1a453d22137bebdb97ec1
fixercoin <khanbabusargodhaa@gmail.com>
2025-10-07 14:43:55 -0700

    Update ping.js

--- functions/api/ping.js
+++ functions/api/ping.js
@@ -1,4 +1,5 @@
-export const onRequest = async ({request}) => {
-  if (request.method === 'OPTIONS') return new Response(null, {status:204, headers: {'Access-Control-Allow-Origin':'*','Access-Control-Allow-Methods':'GET,POST,OPTIONS','Access-Control-Allow-Headers':'Content-Type,Authorization'}});
-  return new Response(JSON.stringify({message: 'ping'}), {status:200, headers: {'Content-Type':'application/json','Access-Control-Allow-Origin':'*'}});
-};
+export async function onRequestGet() {
+  return new Response(JSON.stringify({ status: "ok", message: "pong" }), {
+    headers: { "Content-Type": "application/json" },
+  });
+}

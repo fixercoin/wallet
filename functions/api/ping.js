39a1521699d9d96a05928e3ce5b773e9453de254
fixercoin <khanbabusargodhaa@gmail.com>
2025-10-07 15:46:20 -0700

    Add files via upload

--- functions/api/ping.js
+++ functions/api/ping.js
@@ -1,5 +1,4 @@
-export async function onRequestGet() {
-  return new Response(JSON.stringify({ status: "ok", message: "pong" }), {
-    headers: { "Content-Type": "application/json" },
-  });
-}
+export const onRequest = async ({request}) => {
+  if (request.method === 'OPTIONS') return new Response(null, {status:204, headers: {'Access-Control-Allow-Origin':'*','Access-Control-Allow-Methods':'GET,POST,OPTIONS','Access-Control-Allow-Headers':'Content-Type,Authorization'}});
+  return new Response(JSON.stringify({message: 'ping'}), {status:200, headers: {'Content-Type':'application/json','Access-Control-Allow-Origin':'*'}});
+};

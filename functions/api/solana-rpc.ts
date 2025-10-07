aa0201bc5aa21e5ae83c97343e7867186a491988
fixercoin <khanbabusargodhaa@gmail.com>
2025-10-07 15:13:43 -0700

    Update solana-rpc.ts

--- functions/api/solana-rpc.ts
+++ functions/api/solana-rpc.ts
@@ -1,19 +1,15 @@
 export async function onRequestPost({ request }) {
-  try {
-    const body = await request.json();
-    const response = await fetch("https://api.mainnet-beta.solana.com", {
+  const body = await request.json();
+  const response = await fetch(
+    "https://solana-mainnet.g.alchemy.com/v2/3Z99FYWB1tFEBqYSyV60t-x7FsFCSEjX",
+    {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(body),
-    });
-    const data = await response.json();
-    return new Response(JSON.stringify(data), {
-      headers: { "Content-Type": "application/json" },
-    });
-  } catch (error) {
-    return new Response(JSON.stringify({ error: error.message }), {
-      status: 500,
-      headers: { "Content-Type": "application/json" },
-    });
-  }
+    }
+  );
+  return new Response(await response.text(), {
+    status: response.status,
+    headers: { "Content-Type": "application/json" },
+  });
 }

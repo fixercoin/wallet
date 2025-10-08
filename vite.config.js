273564ae7417798a38639cc33e32988f6f3fa2a6
fixercoin <khanbabusargodhaa@gmail.com>
2025-10-08 01:28:03 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,17 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  base: "./", // ✅ ensures assets work on Pages
+  base: "./",
+  plugins: [react()],
   build: {
     outDir: "dist/spa",
     emptyOutDir: true,
   },
-  plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./client"),
+      "@utils": path.resolve(__dirname, "utils"),
     },
   },
 });

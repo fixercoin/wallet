855df543f3e2fcbac2ce7106f667f234060818d1
fixorium <khanbabusargodhaa@gmail.com>
2025-11-04 15:35:24 -0800

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,24 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  base: "./",
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "client"),       // UI source
-      "@shared": path.resolve(__dirname, "shared"), // Shared logic
-      "@lib": path.resolve(__dirname, "shared/lib") // Replaces "@/lib"
-    },
-  },
-  build: {
-    outDir: "dist/spa",
-    emptyOutDir: true,
-  },
   server: {
     host: true,
     port: 5173,

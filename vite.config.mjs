c4b3535f3d32a211d4a70cf819a9513593235078
fixorium <khanbabusargodhaa@gmail.com>
2025-11-21 01:08:11 +0500

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -20,6 +20,11 @@ export default defineConfig({
 
   server: {
     host: true,
-    port: 5173
+    port: 5173,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "*",
+      "Access-Control-Allow-Methods": "*"
+    }
   }
 });
